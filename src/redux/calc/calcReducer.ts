25e35ea3c9a5db5a9a06df5ab0017bb5e2b04c20
Oustinger <oustinger@gmail.com>
2021-09-07 23:45:41 +0500

    REFACTORING: calcReducer.js add return interfaces for action funcs

--- src/redux/calc/calcReducer.ts
+++ src/redux/calc/calcReducer.ts
@@ -1,19 +1,27 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import calculateResult from '../../calculatorFunctional/calculateResult/calculateResult';
-import calcOperations, { CalcOperationsInterface } from '../../calculatorFunctional/operations/calculateOperations/calcOperations';
-import defaultOperations, { DefaultOperationsInterface } from '../../calculatorFunctional/operations/defaultOperations/defaultOperations';
-import numbersInputs, { NumbersInputsInterface } from '../../calculatorFunctional/operations/numbersInputs/numbersInputs';
-import parenthesesOperations, { ParenthesesInterface } from '../../calculatorFunctional/operations/parentheses/parentheses';
+import calcOperations, {
+    CalcOperationsInterface,
+} from '../../calculatorFunctional/operations/calculateOperations/calcOperations';
+import defaultOperations, {
+    DefaultOperationsInterface,
+} from '../../calculatorFunctional/operations/defaultOperations/defaultOperations';
+import numbersInputs, {
+    NumbersInputsInterface,
+} from '../../calculatorFunctional/operations/numbersInputs/numbersInputs';
+import parenthesesOperations, {
+    ParenthesesInterface,
+} from '../../calculatorFunctional/operations/parentheses/parentheses';
 
 interface calcReducerStateInterface {
-    input: string,
-    expression: string | null,
-    numbersInputs: NumbersInputsInterface,
-    defaultOperations: DefaultOperationsInterface,
-    parenthesesOperations: ParenthesesInterface,
-    calcOperations: CalcOperationsInterface,
-    error: string | null,
-};
+    input: string;
+    expression: string | null;
+    numbersInputs: NumbersInputsInterface;
+    defaultOperations: DefaultOperationsInterface;
+    parenthesesOperations: ParenthesesInterface;
+    calcOperations: CalcOperationsInterface;
+    error: string | null;
+}
 
 const initialState: calcReducerStateInterface = {
     input: '',
@@ -25,36 +33,37 @@ const initialState: calcReducerStateInterface = {
     error: null,
 };
 
-type MyPayloadAction = PayloadAction<{ [key: string]: string }>;
+export type TCalcReducerPayloadAction = PayloadAction<{ [key: string]: string } | undefined>;
 
 export const calcSlice = createSlice({
     name: 'calc',
     initialState,
     reducers: {
-        setInputAction: (state, action: MyPayloadAction) => ({ ...state, ...action.payload }),
-        addSymbolAction: (state, action: MyPayloadAction) => (
-            { ...state, input: `${state.input}${action.payload ? action.payload.symbol : ''}` }
-        ),
+        setInputAction: (state, action: TCalcReducerPayloadAction) => ({ ...state, ...action.payload }),
+        addSymbolAction: (state, action: TCalcReducerPayloadAction) => ({
+            ...state,
+            input: `${state.input}${action.payload ? action.payload.symbol : ''}`,
+        }),
         calculateAction: (state) => {
             try {
                 const expression = state.input;
                 const result = calculateResult(expression);
-                return ({ ...state, input: result, expression, error: null });
+                return { ...state, input: result, expression, error: null };
             } catch (error) {
-                return ({ ...state, error: error.message });
+                return { ...state, error: error.message };
             }
         },
         cleanAction: (state) => ({ ...state, input: '', expression: null, error: null }),
         deleteLastSymbolAction: (state) => ({ ...state, input: state.input.slice(0, state.input.length - 1) }),
-    }
+    },
 });
 
 const { setInputAction, addSymbolAction, calculateAction, cleanAction, deleteLastSymbolAction } = calcSlice.actions;
 
-export const setInput = (input: string) => setInputAction({ input });
-export const addSymbol = (symbol: string) => addSymbolAction({ symbol });
-export const calculate = () => calculateAction();
-export const clean = () => cleanAction();
-export const deleteLastSymbol = () => deleteLastSymbolAction();
+export const setInput = (input: string): TCalcReducerPayloadAction => setInputAction({ input });
+export const addSymbol = (symbol: string): TCalcReducerPayloadAction => addSymbolAction({ symbol });
+export const calculate = (): TCalcReducerPayloadAction => calculateAction();
+export const clean = (): TCalcReducerPayloadAction => cleanAction();
+export const deleteLastSymbol = (): TCalcReducerPayloadAction => deleteLastSymbolAction();
 
 export default calcSlice.reducer;
